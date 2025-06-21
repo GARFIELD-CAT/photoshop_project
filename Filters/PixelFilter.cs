a59cf16b70ab64e6f4e369581ee1fcfe8e5faa47
Yagunov Denis <spb.jagunov.denis@gmail.com>
2025-06-21 12:34:40 +0300

    Выполнена 1 часть задания Делегирование

--- Filters/PixelFilter.cs
+++ Filters/PixelFilter.cs
@@ -2,9 +2,18 @@ using System;
 
 namespace MyPhotoshop
 {
-    public abstract class PixelFilter<TParameters> : ParametrizedFilter<TParameters>
+    public class PixelFilter<TParameters> : ParametrizedFilter<TParameters>
         where TParameters : IParameters, new()
     {
+        string name;
+        Func<Pixel, TParameters, Pixel> processor;
+
+        public PixelFilter(string name, Func<Pixel, TParameters, Pixel> processor)
+        {
+            this.name = name;
+            this.processor = processor;
+        }
+
         public override Photo Process(Photo original, TParameters parameters)
         {
             var result = new Photo(original.width, original.height);
@@ -12,13 +21,16 @@ namespace MyPhotoshop
             for (int x = 0; x < result.width; x++)
                 for (int y = 0; y < result.height; y++)
                 {
-                    result[x, y] = ProcessPixel(original[x, y], parameters);
+                    result[x, y] = processor(original[x, y], parameters);
                 }
                         
             return result;
         }
 
-        public abstract Pixel ProcessPixel(Pixel original, TParameters parameters);
+        public override string ToString()
+        {
+            return name;
+        }
     }
 }
 
--- Main.cs
+++ Main.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace MyPhotoshop
@@ -9,8 +10,31 @@ namespace MyPhotoshop
 		public static void Main (string[] args)
 		{
             var window = new MainWindow();
-            window.AddFilter(new LighteningFilter());
-            window.AddFilter(new GrayscalerFilter());
+            window.AddFilter(new PixelFilter<LighteningParameters>(
+                "Осветление/затемнение",
+                (original, parameters) => original * parameters.Coefficient
+            ));
+            window.AddFilter(new PixelFilter<EmptyParameters>(
+                "50 оттенков серого / СПб скай",
+                (original, parameters) => {
+                    var lightness = original.R + original.G + original.B;
+                    lightness /= 3;
+
+                    return new Pixel(lightness, lightness, lightness);
+                }
+            ));
+            window.AddFilter(new TransformFilter(
+                "Отразить по горизонтали",
+                size => size,
+                (point, size) => new Point(size.Width - point.X - 1, point.Y)
+             ));
+            window.AddFilter(new TransformFilter(
+                "Повернуть против часовой стрелки",
+                size => new Size(size.Height, size.Width),
+                (point, size) => new Point(point.Y, point.X)
+             ));
+
+
             Application.Run(window);
         }
 	}
