e376884421c0a360ec67f45582515e671db7d357
Yagunov Denis <spb.jagunov.denis@gmail.com>
2025-06-21 13:01:54 +0300

    Выполнена 2 часть задания Делегирование

---
+++ Filters/Transform/RotationParameters.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Reflection;
+
+namespace MyPhotoshop
+{
+	public class RotationParameters: IParameters
+    {
+        public double Angle { get; set; }
+
+        public ParameterInfo[] GetDescription()
+        {
+            return new[]
+            {
+                new ParameterInfo { Name="Eujk", MaxValue=360, MinValue=0, Increment=5, DefaultValue=0 }
+            };
+        }
+
+        public void SetValues(double[] values)
+        {
+            Angle = values[0];
+        }
+    }
+}
+
